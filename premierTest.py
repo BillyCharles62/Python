a8eb72cb1e63b7a025c5780313c06e8e75ba16da
billy.charles <billy.charles.etu@univ-lille.fr>
2021-10-11 06:43:14 +0200

    Create premierTest.py

---
+++ premierTest.py
@@ -0,0 +1,26 @@
+print("hello world")
+
+weapon = "T14 Staff"
+armor = "T11 Robe"
+ring = "Deca"
+ability = "T6 Spell"
+print(weapon+" "+ability+" "+armor+" "+ring)
+
+attack = 25
+defense = 20
+hp = 100
+print(hp - (attack - defense))
+
+monstre = ["slime","snake god","oryx the mad god","janus"]
+monstreTuble = ("slime","snake god","oryx the mad god","janus")
+print(monstre[0])
+print(monstre[-1])
+print(weapon[2])
+print(monstre[0][0])
+monstre.append("nut")
+print(monstre[-1])
+print(len(monstre))
+monstre.sort()
+print(monstre)
+monstre.reverse()
+print(monstre)
